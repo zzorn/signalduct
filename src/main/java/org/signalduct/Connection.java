6f075d99b219c396eb51b341cc64f8d39bc91e6d
zzorn <zzorn@iki.fi>
2014-01-21 04:55:53 +0200

    More test refactoring. Introduced NetworkException.

--- src/main/java/org/signalduct/Connection.java
+++ src/main/java/org/signalduct/Connection.java
@@ -5,8 +5,12 @@ package org.signalduct;
  */
 public interface Connection {
 
-
-    void waitUntilConnected(int timeoutMilliseconds);
+    /**
+     * Blocks until the connection is established, or the specified timeout is reached.
+     * @param timeoutMilliseconds max number of milliseconds to wait for the connection to complete
+     * @throws NetworkException if a connection could not be established within the timeout, or if there was some other problem.
+     */
+    void waitUntilConnected(int timeoutMilliseconds) throws NetworkException;
 
     /**
      * @return true if we are currently connected.
--- src/test/java/org/signalduct/NetworkTest.java
+++ src/test/java/org/signalduct/NetworkTest.java
@@ -11,27 +11,32 @@ public class NetworkTest {
 
     private static final int PORT = 87654;
     private static final String LOCALHOST = "localhost";
+    private static final int TIMEOUT_MILLISECONDS = 1000;
+
     private MockConnectionListener listenerOnServer;
     private MockConnectionListener listenerOnClient;
+    private ClientNetwork clientNetwork;
+    private ServerNetwork serverNetwork;
 
     @Before
     public void setUp() throws Exception {
+
         listenerOnServer = new MockConnectionListener("listener on server");
         listenerOnClient = new MockConnectionListener("listener on client");
+        clientNetwork = new ClientNetwork(listenerOnClient);
+        serverNetwork = new ServerNetwork(PORT, listenerOnServer);
+
+        listenerOnServer.assertReceivedNoEvents();
+        listenerOnClient.assertReceivedNoEvents();
     }
 
     @Test
     public void testConnect() throws Exception {
-        ClientNetwork clientNetwork = new ClientNetwork(listenerOnClient);
-        ServerNetwork serverNetwork = new ServerNetwork(PORT, listenerOnServer);
-
-        listenerOnServer.assertReceivedNoEvents();
-        listenerOnClient.assertReceivedNoEvents();
 
         serverNetwork.startAcceptingConnections();
 
         Connection connectionToServer = clientNetwork.connectToServer(LOCALHOST, PORT);
-        connectionToServer.waitUntilConnected(1000);
+        connectionToServer.waitUntilConnected(TIMEOUT_MILLISECONDS);
 
         listenerOnServer.assertReceivedEventsAre(CONNECTED);
         listenerOnClient.assertReceivedEventsAre(CONNECTED);
